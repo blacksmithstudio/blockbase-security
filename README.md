42cad02d982c6056df30edb6c8eb4805002b1b6d
Alexandre Pereira <alexandrepereira@alexandsmacbook.home>
2017-08-25 19:02:53 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Driver Security for Blockbase
+Compatible with Blockbase Framework
+
+### Version
+0.0.1 alpha
+
+### How to install ?
+Security driver is really easy to use, first install it as any Blockbase driver.
+
+```shell
+$ npm i --save @blacksmithstudio/blockbase-security
+```
+
+### Usage
+TODO
+
+License
+----
+
+(Copyright) 2017 - Alexandre Pereira for Blacksmith S.A.S.
+
+
+**Free Software, Hell Yeah!**
+
+[Node.js]:https://nodejs.org/en
+[NPM]:https://www.npmjs.com
---
+++ driver.js
@@ -0,0 +1,72 @@
+const CryptoJS = require('crypto-js')
+
+/**
+ * Blockbase Security Driver
+ * @namespace app.drivers.security
+ * @author Alexandre Pereira <alex@blacksmith.studio>
+ * @param {Object} app - app namespace to update
+ *
+ * @returns {Object} controller
+ */
+module.exports = (app) => {
+    if(!app.config.has('cryptokey'))
+        return app.drivers.logger.error('Drivers', 'Cannot init authenticator, no valid config')
+
+    return {
+        /**
+         * request processing sub methods (with express)
+         * @memberof app.drivers.security
+         */
+        request : {
+            /**
+             * check if a req is eligible to authentication
+             * by checking the presence of headers etc...
+             * @memberof app.drivers.security.request
+             * @param {Object} req - express normalized request
+             * @param {Object} res - express normalized response
+             * @param {boolean} strict - if strict will stop the process
+             * @returns {Object} token data and check result
+             */
+            authenticable(req, res, strict = false){
+                if(!req.data)
+                    req.data = {
+                        token : req.headers['authorization'] ? req.headers['authorization'].split('Bearer ')[1] : null
+                    }
+
+                if(!req.data.token && strict){
+                    res.status(422).send({ error : 'Missing parameter : header authorization bearer token is required' })
+                    return { data : req.data, check : false }
+                }
+
+                return { data : req.data, check : !req.data.token ? false : true }
+            }
+        },
+
+        /**
+         * token sub methods
+         * @memberof app.drivers.security
+         */
+        token : {
+            /**
+             * encrypt the token
+             * @memberof app.drivers.security.token
+             * @param {*} content - content to put in the token
+             * @returns {string} encrypted token
+             */
+            encrypt(content){
+                return CryptoJS.AES.encrypt(JSON.stringify(content), app.config.get('cryptokey')).toString()
+            },
+
+            /**
+             * decrypt the token
+             * @memberof app.drivers.security.token
+             * @param {string} token - token to decrypt
+             * @returns {Object} decrypted content
+             */
+            decrypt(token){
+                let bytes = CryptoJS.AES.decrypt(token.toString(), app.config.get('cryptokey'))
+                return JSON.parse(bytes.toString(CryptoJS.enc.Utf8))
+            }
+        }
+    }
+}
